ea18536613207ef5f3cc0ef1e8de6b3c0efe3d0a
sowasred <muldurozan@gmail.com>
2020-04-04 02:53:02 -0400

    reacttool try

--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import ReactTooltip from "react-tooltip";
+
 import PropTypes from "prop-types";
 import { WrapperTech, SmallWrapper } from "./Wrapper";
 // import Technology from "./Technology";
